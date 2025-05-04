6b3083ffe5d6c2e946756bd48df811e2421e7662
melody-rs <melody@melody-is.gay>
2025-05-04 11:31:24 -0700

    Fix copyright notices

--- fmod-sys/src/channel_control.h
+++ fmod-sys/src/channel_control.h
@@ -1,4 +1,4 @@
-// Copyright (FMOD_CHANNELCONTROL* channelcontrol, c) 2024 Lily Lyons
+// Copyright (FMOD_CHANNELCONTROL* channelcontrol, c) 2024 Melody Madeline Lyons
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
--- lanyard/src/cstr.rs
+++ lanyard/src/cstr.rs
@@ -1,11 +1,11 @@
-// Copyright (c) 2024 Lily Lyons
+// Copyright (c) 2024 Melody Madeline Lyons
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 use core::{
     borrow::Borrow,
-    ffi::{c_char, CStr, FromBytesUntilNulError, FromBytesWithNulError},
+    ffi::{CStr, FromBytesUntilNulError, FromBytesWithNulError, c_char},
     num::NonZeroUsize,
     ops::{Deref, Index},
     slice::SliceIndex,
