cfdecedb9187f4aaafdb91c1b1086cde1acb0e22
melody-rs <melody@melody-is.gay>
2025-05-23 00:22:24 -0700

    Don't include README.md (can't get it to work when publishing?)

--- fmod-oxide/src/lib.rs
+++ fmod-oxide/src/lib.rs
@@ -1,5 +1,100 @@
 //! fmod-oxide
-#![doc = include_str!("../../README.md")]
+//! Safe rust bindings to the FMOD sound engine.
+//! This crate tries to be as rusty and low-cost as possible, without compromising on any APIs.
+//! Certain APIs, such as loading banks from a pointer, are marked as unsafe, but are still available for use.
+//!
+//! Supports FMOD versions >2.0.2.28 and >2.0.3.07, and Windows/Linux/MacOS/HTML5 platforms.
+//!
+//! Any newer patch-level FMOD versions should compile but might have missing features.
+//!
+//! ### Using this crate
+//!
+//! Due to licensing restrictions this crate can't bundle FMOD, so you'll need to [download](https://www.fmod.com/download) a copy of FMOD yourself.
+//!
+//! Make sure to download from `FMOD Engine` specifically.
+//! ![Download page screenshot](https://github.com/melody-rs/fmod-oxide/blob/main/images/download_page.png?raw=true)
+//!
+//! After downloading FMOD, you have to tell this crate where FMOD is located.
+//! **If you're on Windows and used the FMOD installer, you don't have to worry about this.**
+//!
+//! The easiest way is to create a cargo config in your project's root.
+//!
+//! ```toml
+//! # `.cargo/config.toml`
+//!
+//! [env]
+//! FMOD_SYS_FMOD_DIRECTORY = { value = "<absolute install path here>" }
+//! ```
+//!
+//! You can also specify a relative install path like so:
+//!
+//! ```toml
+//! # `.cargo/config.toml`
+//!
+//! [env]
+//! FMOD_SYS_FMOD_DIRECTORY = { value = "<install path here>", relative = true }
+//! ```
+//!
+//! (not recommended because rust-analyzer won't know this) Alternatively, you can specify `FMOD_SYS_FMOD_DIRECTORY` when building your project:
+//!
+//! `FMOD_SYS_FMOD_DIRECTORY=<path> cargo run`
+//!
+//! ### Cross compilation
+//!
+//! This crate supports cross compilation and will look for a target-specific FMOD install.
+//!
+//! The logic is quite basic at the moment, but it'll check if `<fmod install dir>/<target os>` exists and use that.
+//!
+//! If no target specific directory was found, it'll default to `<fmod install dir>`
+//!
+//! ### Using with webassembly
+//!
+//! Currently only `wasm32-unknown-emscripten` works well.
+//! `wasm32-unknown-unknown` also works in some capacity but you have to essentially reimplement parts of libc and emscripten.
+//!
+//! Unfortunately `wasm-bindgen` doesn't work without patches right now, so your milage may vary
+//!
+//! The setup is roughly the same, except you'll need to add some arguments to `EMCC_FLAGS`.
+//!
+//! You can do this by editing `.cargo/config.toml`:
+//! ```toml
+//! # `.cargo/config.toml`
+//!
+//! [env]
+//! EMCC_CFLAGS="-s EXPORTED_RUNTIME_METHODS=ccall,cwrap,setValue,getValue" # FMOD requires this
+//! ```
+//!
+//! If you're using `wasm32-unknown-unknown`, you'll additionally need to add this until [this issue](https://github.com/rust-lang/rust/issues/138762) is closed.
+//!
+//! ```toml
+//! # `.cargo/config.toml`
+//!
+//! [build]
+//! rustflags="-Zwasm-c-abi=spec"
+//! ```
+//!
+//! See [`web-examples/emscripten`](web-examples/emscripten) for a more detailed example.
+//!
+//! # Memory management & Copy types
+//!
+//! All FMOD objects are Copy, Clone, Send and Sync because it's possible to have multiple references to the same object. (e.g. loading a bank and then retrieving it by its path)
+//! There are a lot of use-cases where you may want to fetch something (like a bank) and never use it again.
+//! Implementing `Drop` to automatically release things would go against that particular use-case, so this crate opts to have manual `release()` methods instead.
+//!
+//! This crate does not currently guard against use-after-frees, *however* using most of FMOD's types (especially FMOD Studio's types) after calling `release()` is safe.
+//! I'm still not 100% sure of what is and isn't safe and I'm actively trying to test this.
+//!
+//! # String types
+//!
+//! fmod-oxide aims to be as zero-cost as possible, and as such, it uses UTF-8 C strings from the `lanyard` crate as its string type.
+//! This means that all FMOD functions take a `&Utf8CStr` instead of a `&str` or `&CStr`.
+//! `&Utf8CStr` is pretty cheap to construct (and can even be done statically with the `c!` macro), so this should not be a problem
+//!
+//! When FMOD returns a string, it will always return a `Utf8CString` (the owned version of `Utf8CStr`) because it's difficult to encode lifetime requirements of FMOD strings.
+//!
+//! This applies to structs like `fmod::studio::AdvancedSettings` which store C strings.
+//! Converting structs like `AdvancedSettings` to their FFI equivalents is done by reference as to not pass ownership of the string to FMOD
+//!
 //! # Basic example
 //! ```ignore
 //! // System creation is unsafe and must be performed prior to any other FMOD operations.
